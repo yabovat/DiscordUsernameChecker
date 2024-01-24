6eac529a17dec58aea301520488aabe81a380912
Yahya Abovat <123842331+yabovat@users.noreply.github.com>
2024-01-23 22:58:48 -0500

    Removed Automated Script Login
    Updated the script so it launches the browser and gives you 30 seconds to login and go to settings, instead of doing it autonomously. This is more efficient as the previous version would have you wait several seconds after each step. Also, this is more secure as your login is no longer kept in plaintext.

--- username.py
+++ username.py
@@ -5,37 +5,17 @@ from selenium.webdriver.common.keys import Keys
 
 driver=webdriver.Firefox()
 
-# Navigate to url
-driver.get("https://discord.com/login?redirect_to=%2Fchannels%2F%40me")
+# Launch FireFox and navigate to the discord login page
+driver.get("https://discord.com/login")
 
-# Wait for 3 seconds to load the webpage completely 
-time.sleep(11) 
-
-username = "<Enter Username Here>"
-password = "<Enter Password Here>"
-driver.find_element(By.XPATH, '//*[@id="uid_5"]').send_keys(username) # Enter Username
-driver.find_element(By.XPATH, '//*[@id="uid_7"]').send_keys(password) # Enter Password
-
-# Click the login button
-driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/div[1]/div[1]/div/div/div/div/form/div[2]/div/div[1]/div[2]/button[2]').click()
-time.sleep(7) 
-
-# Click the settings button
-driver.find_element(By.XPATH, '//*[@id="app-mount"]/div[2]/div[1]/div[1]/div/div[2]/div/div/div/div/div[1]/section/div[2]/div[2]/button[3]').click()
-time.sleep(5) 
-
-# Click the edit username button
-driver.find_element(By.XPATH, '//*[@id="my-account-tab"]/div/div[1]/div[2]/div/div[2]/div/div[2]/button').click()
-time.sleep(6)
-
-# Click new username input button
-driver.find_element(By.XPATH, '//*[@id="app-mount"]/div[2]/div[1]/div[4]/div[2]/div/div/form/div[1]/div[1]/div/div/input').click()
-time.sleep(5)
+# Enter Username/Password and login, click "User Settings", then "Edit" next to username 
+time.sleep(30) # Selenium will wait for 30 Seconds for you to complete the above step
 
 # Clear the current username                           
 driver.find_element(By.XPATH, '//*[@id="app-mount"]/div[2]/div[1]/div[4]/div[2]/div/div/form/div[1]/div[1]/div/div/input').send_keys([Keys.BACKSPACE] * 1000) #clear username
 
-yeahIsAvailable = "Username is available. Nice!" # username is available alert
+# Variable to store Discord's username is available alert
+yeahIsAvailable = "Username is available. Nice!" 
 
 # Append the list of usernames you would like to check to the following array of strings:
 usernames = [] 
@@ -65,7 +45,7 @@ usernames.append("m.")
 
 for str in usernames:
     driver.find_element(By.XPATH, '//*[@id="app-mount"]/div[2]/div[1]/div[4]/div[2]/div/div/form/div[1]/div[1]/div/div/input').send_keys(str) # input username
-    time.sleep(3) # wait for validaiton from discord
+    time.sleep(3) # wait for validaiton from Discord
     isUsernameAvail = driver.find_element(By.XPATH, '//*[@id="app-mount"]/div[2]/div[1]/div[4]/div[2]/div/div/form/div[1]/div[2]/div/div').text # alert XPATH
     if isUsernameAvail == yeahIsAvailable: #check isUsernameAvailable? if yeahIsAvailable print username 
         print(str)
